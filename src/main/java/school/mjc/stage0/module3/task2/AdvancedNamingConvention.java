5640d6b2d4bd0e6223ecbba3dd583038299bf6ec
Vu <nguyentuanvu9x@gmail.com>
2023-08-12 01:15:22 +0300

    Updated: AdvancedNamingConvention.java and CopiedNames.java

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.module3.task2;
 
 public class AdvancedNamingConvention {
-    private static final int adult_age = 18;
+    private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -4,11 +4,15 @@ public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
         variable = 2000;
-        variable = 4000;
         int value = 1000;
-        value = 90000;
+        variable = 4000;
+        int value2 = 3000;
         int variables = 5000;
         variables = 7000;
-        int value2 = 3000;
+        value = 90000;
+        int interger = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        myInteger = 1234567;
     }
 }
